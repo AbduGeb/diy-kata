7def29becee847b01453a0dcff30053e987ae8e6
Abdu <abdu.gebril@gmail.com>
2022-06-30 16:46:29 +0100

    1st diy-kata tests push

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,8 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    const myArr = String(number).split("").map((number)=>{
+        return Number(number)
+      })
+      return myArr.reverse();
+};
 
 module.exports = numberToReversedDigits;
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,22 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+  const catDog = number => {
+    const humanAge = number;
+    let catAge = 0;
+    let dogAge = 0;
+    for (let i = 1; i < humanAge.length; i++) {
+      if (humanAge === 1) {
+        catAge = 15;
+        dogAge = 15;
+      } else if (humanAge === 2) {
+        catAge = 24;
+        dogAge = 24;
+      } else if (humanAge >= 3) {
+        catAge += 4;
+        dogAge += 5;
+      }
+    }
+    return [humanAge, catAge, dogAge];
+  };
+};
 
 module.exports = humanCatDogYears;
